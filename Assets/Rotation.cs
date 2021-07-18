c58580c6bee17f639f293a69ef803af22b917acf
EF <eorf256@gmail.com>
2021-07-19 01:55:07 +0900

    first commit

---
+++ Assets/Rotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private float rot = 0.05f;
+    
+    void Update()
+    {
+        this.transform.Rotate(0, rot, 0);
+    }
+}
